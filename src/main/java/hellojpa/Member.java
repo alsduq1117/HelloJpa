c053ee9148fbadb60f3333e133916ed6821168c8
Minyeob Kim <alsduq1117@naver.com>
2022-09-21 01:11:20 +0900

    @mappedsuperclass commit

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.concurrent.locks.Lock;
 
 @Entity
-public class Member {
+public class Member extends BaseEntitiy {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
